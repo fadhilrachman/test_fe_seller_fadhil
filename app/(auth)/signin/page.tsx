3085846d236be811379d8d8a87d59b5d42b59977
fadhilrachman <muhfadhilrachman@gmail.com>
2024-11-06 11:02:10 +0700

    feat: change login page

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,75 +1,65 @@
 'use client';
 import Link from 'next/link';
-import UserAuthForm from '@/components/forms/user-auth-form';
-import { buttonVariants } from '@/components/ui/button';
-import { cn } from '@/lib/utils';
 import SignInForm from '@/features/auth/signin-form';
-
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem
+} from '@/components/ui/carousel';
+import Autoplay from 'embla-carousel-autoplay';
+import Content1 from '@/features/auth/carousel-content/content1';
+import Content2 from '@/features/auth/carousel-content/content2';
+import Content3 from '@/features/auth/carousel-content/content3';
 export default function AuthenticationPage() {
   return (
-    <div className="relative h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-      <Link
-        href="/examples/authentication"
-        className={cn(
-          buttonVariants({ variant: 'ghost' }),
-          'absolute right-4 top-4 hidden md:right-8 md:top-8'
-        )}
-      >
-        Login
-      </Link>
-      <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
-        <div className="absolute inset-0 bg-zinc-900" />
-        <div className="relative z-20 flex items-center text-lg font-medium">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="mr-2 h-6 w-6"
-          >
-            <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
-          </svg>
-          Logo
-        </div>
-        <div className="relative z-20 mt-auto">
-          <blockquote className="space-y-2">
-            <p className="text-lg">
-              &ldquo;This library has saved me countless hours of work and
-              helped me deliver stunning designs to my clients faster than ever
-              before.&rdquo;
-            </p>
-            <footer className="text-sm">Sofia Davis</footer>
-          </blockquote>
-        </div>
+    <div className="relative h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-5 lg:px-0">
+      <div className=" col-span-3 h-[100vh] rounded-md p-6">
+        <Carousel
+          className="h-full"
+          plugins={[
+            Autoplay({
+              delay: 2000
+            })
+          ]}
+        >
+          <CarouselContent className="h-full">
+            <CarouselItem className="h-[680px]">
+              <Content1 />
+            </CarouselItem>
+            <CarouselItem>
+              <Content2 />
+            </CarouselItem>
+            <CarouselItem>
+              <Content3 />
+            </CarouselItem>
+          </CarouselContent>
+        </Carousel>
       </div>
-      <div className="flex h-full items-center p-4 lg:p-8">
-        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-          <div className="flex flex-col space-y-2 text-center">
-            <h1 className="text-2xl font-semibold tracking-tight">
-              Create an account
+      <div className="col-span-2 flex h-full items-center border p-4 lg:p-8">
+        <div className=" mx-auto flex  w-[420px] flex-col justify-center space-y-6">
+          <div className="flex flex-col space-y-2 border-b pb-2 text-left">
+            <h1 className="text-left text-2xl font-semibold tracking-tight">
+              Login
             </h1>
             <p className="text-sm text-muted-foreground">
-              Enter your email below to create your account
+              Masukkan email Anda di bawah ini untuk masuk akun Anda
             </p>
           </div>
           <SignInForm />
-          <p className="px-8 text-center text-sm text-muted-foreground">
-            By clicking continue, you agree to our{' '}
+          <p className=" text-center text-sm text-muted-foreground">
+            Dengan mengklik lanjutkan Anda setuju dengan kami{' '}
             <Link
               href="/terms"
               className="underline underline-offset-4 hover:text-primary"
             >
-              Terms of Service
+              Ketentuan Layanan
             </Link>{' '}
-            and{' '}
+            dan{' '}
             <Link
               href="/privacy"
               className="underline underline-offset-4 hover:text-primary"
             >
-              Privacy Policy
+              Kebijakan Privasi
             </Link>
             .
           </p>
---
+++ features/auth/carousel-content/content2.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Content2 = () => {
+  return (
+    <div className="relative mx-4 hidden h-full  flex-col rounded-md bg-muted bg-[url('/auth/carousel/content2.jpg')] bg-cover bg-center p-10  text-white dark:border-r lg:flex">
+      <div className="relative z-20 flex items-center text-lg font-medium">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="mr-2 h-6 w-6"
+        >
+          <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
+        </svg>
+        Logo
+      </div>
+      <div className="relative z-20 mt-auto">
+        <blockquote className="space-y-2">
+          <p className="text-lg">
+            &ldquo;Solusi HRIS Terintegrasi untuk Tim yang Lebih Solid.&rdquo;
+          </p>
+          {/* <footer className="text-sm">Sofia Davis</footer> */}
+        </blockquote>
+      </div>
+    </div>
+  );
+};
+
+export default Content2;
